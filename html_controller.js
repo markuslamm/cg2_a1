f2acb327e18bea1c1aaaf1478fc47cfa6660b208
Markus Lamm <markus.lamm@googlemail.com>
2013-04-21 15:46:26 +0200

    refactor ui

--- html_controller.js
+++ html_controller.js
@@ -14,38 +14,28 @@ define([ "jquery", "straight_line", "circle", "parametric_curve" ],
 		(function($, StraightLine, Circle, ParametricCurve) {
 
 			"use strict";
-
 			/*
 			 * define callback functions to react to changes in the HTML page
 			 * and provide them with a closure defining context and scene
 			 */
 			var HtmlController = function(context, scene, sceneController) {
 
-				var selectedObject = null;
-
 				// generate random X coordinate within the canvas
-				var randomX = function() {
-					return Math.floor(Math.random()
-							* (context.canvas.width - 10)) + 5;
-				};
+				var randomX = function() { return Math.floor(Math.random() * (context.canvas.width - 10)) + 5; };
 
 				// generate random Y coordinate within the canvas
-				var randomY = function() {
-					return Math.floor(Math.random()
-							* (context.canvas.height - 10)) + 5;
-				};
+				var randomY = function() { return Math.floor(Math.random() * (context.canvas.height - 10)) + 5; };
 
+				// generate random radius within the canvas with a minimal radius
 				var randomRadius = function() {
 					var minRadius = 20;
-					var radius = Math.floor(Math.random()
-							* (context.canvas.height / 2)) + 1;
+					var radius = Math.floor(Math.random() * (context.canvas.height / 2)) + 1;
 					console.log("Calculated random radius: " + radius);
 					return radius < minRadius ? minRadius : radius;
 				}
 
 				// generate random color in hex notation
 				var randomColor = function() {
-
 					// convert a byte (0...255) to a 2-digit hex string
 					var toHex2 = function(byte) {
 						var s = byte.toString(16); // convert to hex string
@@ -53,7 +43,6 @@ define([ "jquery", "straight_line", "circle", "parametric_curve" ],
 							s = "0" + s; // pad with leading 0
 						return s;
 					};
-
 					var r = Math.floor(Math.random() * 25.9) * 10;
 					var g = Math.floor(Math.random() * 25.9) * 10;
 					var b = Math.floor(Math.random() * 25.9) * 10;
@@ -61,99 +50,119 @@ define([ "jquery", "straight_line", "circle", "parametric_curve" ],
 					// convert to hex notation
 					return "#" + toHex2(r) + toHex2(g) + toHex2(b);
 				};
-
+				
+				$("#commonSection").hide();
+				$("#radiusSection").hide();
+				$("#curveSection").hide();
+				
 				/*
-				 * UI elements
+				 * callback when object is selected
 				 */
-				var mainEditArea = $("#editArea");
-				var strokeColor = $("#stroke_color");
-				var strokeWidth = $("#stroke_color");
-				
-				var radiusArea = $("#radiusSection")
-				var radius = $("#radius");
-				
-				var curveArea = $("#curveSection");
-				var xFunction = $("#xFunction");
-				var yFunction = $("#yFunction");
-				var tMin = $("#tMin");
-				var tMax = $("#tMax");
-				var segments = $("#segments");
-				
-				mainEditArea.hide();
+				var onSelectionCallback = function() {
+					console.log("Invoking onSelectCallback");
+					var selectedObject = sceneController.getSelectedObject();
+					
+					$("#commonSection").show();
+					$("#stroke_width").attr("value", selectedObject.lineStyle.width);
+					$("#stroke_color").attr("value", selectedObject.lineStyle.color);
+					
+					/*
+					 * display elements for straight line
+					 */
+					var buildLineSection = function() {
+						$("#radiusSection").hide();
+						$("#curveSection").hide();
+					}
+					
+					/*
+					 * display elements for circle
+					 */
+					var buildCircleSection = function() {
+						$("#radiusSection").show();
+						$("#curveSection").hide();
+						$("#radius").attr("value", selectedObject.radius);
+						
+					};
+					
+					/*
+					 * display elements for parametric curve
+					 */
+					var buildCurveSection = function() {
+						$("#radiusSection").hide();
+						$("#curveSection").show();
+						var f_FuncString = selectedObject.f_Function.toString();
+						var g_FuncString = selectedObject.g_Function.toString();
+						console.log("f(t): " + f_FuncString);
+						console.log("g(t): " + g_FuncString);
 
-				var displayUIElements = function(obj) {
-					var isCircle = obj instanceof Circle;
-					var isParametricCurve = obj instanceof ParametricCurve;
-					var isStraightLine = obj instanceof StraightLine;
-					var color = obj.lineStyle.color;
-					var width = obj.lineStyle.width;
-					strokeColor.attr("value", color);
-					strokeWidth.attr("value", width);
-					if (isCircle) {
-						console.log("Display for circle ");
-						var r = obj.radius;
-						radius.attr("value", r);
-						radiusArea.show();
-						curveArea.hide();
-					} else if (isParametricCurve) {
-						console.log("Display for parametric curve ");
-						curveArea.show();
-						radiusArea.hide();
-
-					} else if (isStraightLine) {
-						console.log("Display for straight line ");
-						radiusArea.hide();
-						curveArea.hide();
-					} else {
+						$("#xFunction").attr("value", selectedObject.xFunction);
+						$("#yFunction").attr("value", selectedObject.yFunction);
+						$("#tMin").attr("value", selectedObject.tMin);
+						$("#tMax").attr("value", selectedObject.tMax);
+						$("#segments").attr("value", selectedObject.segments);
+					};
+					
+					/*
+					 * check object type
+					 */
+					if(selectedObject instanceof StraightLine) {
+						console.log("StraightLine selected ");
+						buildLineSection();
+					}
+					else if(selectedObject instanceof Circle) {
+						console.log("Circle selected ");
+						buildCircleSection();
+					}
+					else if(selectedObject instanceof ParametricCurve) {
+						console.log("ParametricCurve selected ");
+						buildCurveSection();
+						
+					}
+					else {
 						throw new Error(
 								"Unknown obj type. Cannot display ui elements")
 					}
 				};
+				sceneController.onSelection(onSelectionCallback);
 
-				var onSelectCallback = function(obj) {
-					console.log("Invoking onSelectCallback");
-					selectedObject = obj;
-					displayUIElements(selectedObject);
-				};
+				/*
+				 * UI elements
+				 */
 
-				sceneController.onSelection(onSelectCallback);
-
-				$("#stroke_color").change(function() {
-					var newColorValue = $("#stroke_color").attr("value");
-					console.log("new color value: [" + newColorValue + "]");
-					if (selectedObject != null) {
-						selectedObject.lineStyle.color = newColorValue;
-						sceneController.select(selectedObject);
-					}
-				});
-
-				$("#stroke_width").change(function() {
-					var newStrokeValue = $("#stroke_width").attr("value");
-					console.log("new stroke value: [" + newStrokeValue + "]");
-					if (selectedObject != null) {
-						selectedObject.lineStyle.width = newStrokeValue;
-						sceneController.select(selectedObject);
-					}
-				});
-
-				$("#radius").change(function() {
-					var newRadiusValue = $("#radius").attr("value");
-					console.log("new radius value: [" + newRadiusValue + "]");
-					if (selectedObject != null) {
-						selectedObject.radius = newRadiusValue;
-						sceneController.select(selectedObject);
-					}
-
-				});
+//				$("#stroke_color").change(function() {
+//					var selectedObject = sceneController.getSelectedObject();
+//					var newColorValue = $("#stroke_color").attr("value");
+//					console.log("new color value: [" + newColorValue + "]");
+//					selectedObject.lineStyle.color = newColorValue;
+//					sceneController.select(selectedObject);
+//					}
+//				});
+//
+//				$("#stroke_width").change(function() {
+//					var newStrokeValue = $("#stroke_width").attr("value");
+//					console.log("new stroke value: [" + newStrokeValue + "]");
+//					if (selectedObject != null) {
+//						selectedObject.lineStyle.width = newStrokeValue;
+//						sceneController.select(selectedObject);
+//					}
+//				});
+//
+//				$("#radius").change(function() {
+//					var newRadiusValue = $("#radius").attr("value");
+//					console.log("new radius value: [" + newRadiusValue + "]");
+//					if (selectedObject != null) {
+//						selectedObject.radius = newRadiusValue;
+//						sceneController.select(selectedObject);
+//					}
+//
+//				});
 
 				/*
 				 * event handler for "new line button".
 				 */
 				$("#btnNewLine").click(
 						(function() {
-
 							// create the actual line and add it to the scene
-
 							var style = {
 								width : Math.floor(Math.random() * 3) + 1,
 								color : randomColor()
@@ -169,8 +178,6 @@ define([ "jquery", "straight_line", "circle", "parametric_curve" ],
 							sceneController.deselect();
 							sceneController.select(line); // this will also
 															// redraw
-							$("#editArea").show();
-
 						}));
 
 				/*
@@ -196,45 +203,29 @@ define([ "jquery", "straight_line", "circle", "parametric_curve" ],
 							sceneController.deselect();
 							sceneController.select(circle); // this will also
 															// redraw
-							$("#editArea").show();
 						}));
 
 				$("#btnNewParametricCurve").click(
-						(function() {
-							mainEditArea.show();
-							radiusArea.hide();
-							curveArea.show();
-						}));
-
-				$("#btnPlotCurve").click(
-						(function() {
-							
+						(function() {							
+							//initial values
 							var style = {
-								width : Math.floor(Math.random() * 3) + 1,
-								color : randomColor()
-							};
+									width : Math.floor(Math.random() * 3) + 1,
+									color : randomColor()
+								};
 							
-							var xFormular = xFunction.attr("value");
-							var yFormular = yFunction.attr("value");
+							/*
+							 * Initial values for curve
+							 */
+//							var f_Function = function() { return return 350 + 100 * Math.sin(t); };
+//							var g_Function = function() { return return 150 + 100 * Math.cos(t); };
+//							var tMin = 0;
+//							var tMax = 2 * Math.PI;
+//							var segments = 20;
 							
-							var initX = function(t) {
-								var result = eval(xFormular);
-								console.log("x eval: " + result)
-								return result;
-							}
-							
-							var initY = function(t) {
-								var result = eval(yFormular);
-								console.log("y eval: " + result)
-								return result;
-							}
-							
-							
-
-							var paramCurve = new ParametricCurve(initX, initY, 1, 100, 100, style);
+							var paramCurve = new ParametricCurve(style);
 							scene.addObjects([ paramCurve ]);
 							sceneController.deselect();
-							sceneController.select(paramCurve);
+							sceneController.select(paramCurve);							
 						}));
 			};
 
--- parametric_curve.js
+++ parametric_curve.js
@@ -2,63 +2,34 @@
  * 
  */
 /* requireJS module definition */
-define(["util", "vec2", "scene", "point_dragger"], 
-       (function(Util,vec2,Scene,PointDragger) {
-       
-    "use strict";
-    
-    var ParametricCurve = function(xFunction, yFunction, tMin, tMax, segments, lineStyle) {
-    	console.log("creating ParametricCurve.");
-    	this.xFunction = xFunction;
-    	this.yFunction = yFunction;
-    	this.tMin = tMin;
-    	this.tMax = tMax;
-    	this.segments = segments;
-//    	console.log("xFunction: " + xFunction);
-//    	console.log("yFunction: " + yFunction);
-//    	console.log("tMin: " + tMin);
-//    	console.log("tMax: " + tMax);
-//    	console.log("segments: " + segments);
-    	this.lineStyle = lineStyle || { width: "2", color: "#0000AA" };
-    };
-    
-    ParametricCurve.prototype.draw = function(context) {
-    	console.log("ParametricCurve.prototype.draw()");
-    	var stepLength = (this.tMax - this.tMin) / this.segments;
-    	var min = this.tMin;
-    	var max = this.tMax;
-    	var i;
-    	console.log("stepLength: " + stepLength);
-    	var points = [];
-    	var startPoint = [this.xFunction(min), this.yFunction(min)];
-    	console.log("startPoint: " + startPoint);
-    	
-    	for(i = min; i <= max; i += stepLength) {
-    		//console.log("step " + i + ", x: " + this.xFunction(i) + ", y: " + this.yFunction(i));
-    		points.push([this.xFunction(i), this.yFunction(i)]);
-    		context.beginPath();
-    		context.arc(this.xFunction(i), this.yFunction(i), 2, 0, 2 * Math.PI, false),
-        	
-       	 // set drawing style
-           context.lineWidth = this.lineStyle.width;
-           context.strokeStyle = this.lineStyle.color;
-           
-        // actually start drawing
-           context.stroke(); 
-    	}
-	}
-    
-    ParametricCurve.prototype.isHit = function(context, position) {
-    	console.log("ParametricCurve.prototype.isHit()");
-    	return false;
-	}
-    
+define([ "util", "vec2", "scene" ], (function(Util, vec2, Scene, PointDragger) {
+
+	"use strict";
+
+	var ParametricCurve = function(lineStyle) {
+		console.log("creating ParametricCurve.");
+		this.f_Function = function() { return 350 + 100 * Math.sin(t); };
+		this.g_Function = function() { return 150 + 100 * Math.cos(t); };
+		this.tMin = 0;
+		this.tMax = 2 * Math.PI;
+		this.segments = 20;
+		this.lineStyle = lineStyle || { width : "2", color : "#0000AA"};
+	};
+
+	ParametricCurve.prototype.draw = function(context) {
+		console.log("ParametricCurve.prototype.draw()");
+	};
+
+	ParametricCurve.prototype.isHit = function(context, position) {
+		console.log("ParametricCurve.prototype.isHit()");
+	};
+	
     ParametricCurve.prototype.createDraggers = function() {
         var draggers = [];
    	 	return draggers;
        
    };
-    
-    return ParametricCurve;
+
+	return ParametricCurve;
 
 })); // define
